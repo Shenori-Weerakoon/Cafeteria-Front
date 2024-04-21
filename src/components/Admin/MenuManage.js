a81b6d3a935fbdc7998e583608475c98a7b0e734
shenori <it22150448@my.sliit.lk>
2024-04-21 17:00:59 +0530

    implement delete operation in menumanage

--- src/components/Admin/MenuManage.js
+++ src/components/Admin/MenuManage.js
@@ -48,6 +48,22 @@ const handleUpdateMenuItem = (selectedMenuItem) => {
     });
 }
 
+const handleDeleteMenu = (itemId) => {
+    axios.delete(global.APIUrl + "/menuItem/delete/" + itemId).then(() => {
+        window.location.href = "/MenuManage";
+
+    }).catch((err) => {
+        Swal.fire({
+            title: "Error!",
+            text: "Menu Not Delete",
+            icon: 'error',
+            confirmButtonText: "OK",
+            type: "success"
+        })
+    })
+
+};
+
 
 const columns = [
     { field: 'itemId', headerName: 'Item ID', width: 150 },
