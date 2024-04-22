be4ce7b85f4f9454a106f1f38ac68fcc51dc9c3e
amashisathisma@gmail.com <amashisathisma@gmail.com>
2024-04-22 11:46:45 +0530

    change bg colour

--- src/components/Admin/Customer.js
+++ src/components/Admin/Customer.js
@@ -37,7 +37,7 @@ const Customer = () => {
     return (
         <div style={{ display: 'flex', height: '100vh', maxWidth: '161vh' }}>
             <Sidebar />
-            <div style={{ flexGrow: 1, padding: 20, backgroundColor: '#ecf0f1', display: 'flex', flexDirection: 'column' }}>
+            <div style={{ flexGrow: 1, padding: 20, backgroundColor: '#ffffff', display: 'flex', flexDirection: 'column' }}>
                 <AppBar position="static" sx={{ backgroundColor: '#1c2331', boxShadow: 'none' }}>
                     <Toolbar>
                         <Typography variant="h6" component="div">
