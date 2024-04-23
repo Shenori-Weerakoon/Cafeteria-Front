6f1b4a4af1e3316b02ec755cb7afc72a0c523f9d
shenori <it22150448@my.sliit.lk>
2024-04-23 14:40:08 +0530

    use styles

--- src/components/Main/SideBar.js
+++ src/components/Main/SideBar.js
@@ -14,10 +14,15 @@ import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 import AllInboxIcon from '@mui/icons-material/AllInbox';
 import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 import ZperxLogo from '../Images/Zperx.png';
+import { Link, useLocation} from 'react-router-dom';
 
 const SideBar = () => {
   const [selectedItem, setSelectedItem] = useState('Dashboard');
   const hradmin = sessionStorage.getItem("hradmin");
+  const location = useLocation();
+  const isActive = (path) => {
+    return location.pathname === path ? { color: '#FFA500', fontWeight: 'bold' } : { color: '#fff' };
+  };
 
 
   useEffect(() => {
@@ -50,110 +55,112 @@ const SideBar = () => {
           '& .MuiDrawer-paper': {
             width: 240,
             boxSizing: 'border-box',
-            backgroundColor: '#009637', 
-            color: '#ffffff', 
+            backgroundColor: '#009637',
+            color: '#ffffff',
           },
         }}
       >
         <Toolbar sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 100 }}>
+        <Link to="/" style={{ ...isActive('/'), textDecoration: 'none' }}>Caravan <span style={{ fontFamily: 'Zekton, sans-serif' }}>Fresh</span>
+          </Link>
           <Avatar
             alt="Company Logo"
             src={ZperxLogo}
-            sx={{ width: 80, height: 80, border: '2px solid yellow' }} 
+            sx={{ width: 80, height: 80, border: '2px solid yellow' }}
           />
         </Toolbar>
         <List>
-        {hradmin === 'true' ? (
-          <>
-          <ListItem
-            button
-            selected={selectedItem === 'Employee'}
-            onClick={() => handleItemClick('Employee')}
-            sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
-          >
-            <ListItemIcon>
-              <BadgeIcon sx={{ color: '#ecf0f1' }} /> 
-            </ListItemIcon>
-            <ListItemText primary="Employee" />
-          </ListItem>
-          <ListItem
-            button
-            selected={selectedItem === 'MenuManage'}
-            onClick={() => handleItemClick('MenuManage')}
-            sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
-          >
-            <ListItemIcon>
-              <MenuOpenIcon sx={{ color: '#ecf0f1' }} /> 
-            </ListItemIcon>
-            <ListItemText primary="Menu Management" />
-          </ListItem>
-          </>
-        ):(  
-          <>
-          <ListItem
-            button
-            selected={selectedItem === 'InventoryManage'}
-            onClick={() => handleItemClick('InventoryManage')}
-            sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
-          >
-            <ListItemIcon>
-              <InventoryIcon sx={{ color: '#ecf0f1' }} /> 
-            </ListItemIcon>
-            <ListItemText primary="Inventory Management" />
-          </ListItem>
-          <ListItem
-            button
-            selected={selectedItem === 'SupportTicket'}
-            onClick={() => handleItemClick('SupportTicket')}
-            sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
-          >
-            <ListItemIcon>
-              <ConfirmationNumberIcon sx={{ color: '#ecf0f1' }} /> 
-            </ListItemIcon>
-            <ListItemText primary="Support Ticket Management" />
-          </ListItem>
-          <ListItem
-            button
-            selected={selectedItem === 'PromotionManage'}
-            onClick={() => handleItemClick('PromotionManage')}
-            sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
-          >
-            <ListItemIcon>
-              <LocalOfferIcon sx={{ color: '#ecf0f1' }} /> 
-            </ListItemIcon>
-            <ListItemText primary="Promotion Management" />
-          </ListItem>
-          <ListItem
-            button
-            selected={selectedItem === 'OrderPaymentManage'}
-            onClick={() => handleItemClick('OrderPaymentManage')}
-            sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
-          >
-            <ListItemIcon>
-              <AllInboxIcon sx={{ color: '#ecf0f1' }} /> 
-            </ListItemIcon>
-            <ListItemText primary="Order Payment Management" />
-          </ListItem>
-          <ListItem
-          button
-          selected={selectedItem === 'OrderPaymentManage'}
-          onClick={() => handleItemClick('OrderPaymentManage')}
-          sx={{ '&:hover': { backgroundColor: '#2c3e50' } }}
-        >
-          <ListItemIcon>
+          {hradmin === 'true' ? (
+            <>
+              <ListItem
+                button
+                selected={selectedItem === 'Employee'}
+                onClick={() => handleItemClick('Employee')}
+                sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
+              >
+                <ListItemIcon>
+                  <BadgeIcon sx={{ color: '#ecf0f1' }} />
+                </ListItemIcon>
+                <ListItemText primary="Employee" />
+              </ListItem>
+              <ListItem
+                button
+                selected={selectedItem === 'Customer'}
+                onClick={() => handleItemClick('Customer')}
+                sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
+              >
+                <ListItemIcon>
+                  <BadgeIcon sx={{ color: '#ecf0f1' }} />
+                </ListItemIcon>
+                <ListItemText primary="Customer" />
+              </ListItem>
+            </>
+          ) : (
+            <>
+              <ListItem
+                button
+                selected={selectedItem === 'MenuManage'}
+                onClick={() => handleItemClick('MenuManage')}
+                sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
+              >
+                <ListItemIcon>
+                  <MenuOpenIcon sx={{ color: '#ecf0f1' }} />
+                </ListItemIcon>
+                <ListItemText primary="Menu Management" />
+              </ListItem>
+              <ListItem
+                button
+                selected={selectedItem === 'InventoryManage'}
+                onClick={() => handleItemClick('InventoryManage')}
+                sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
+              >
+                <ListItemIcon>
+                  <InventoryIcon sx={{ color: '#ecf0f1' }} />
+                </ListItemIcon>
+                <ListItemText primary="Inventory Management" />
+              </ListItem>
+              <ListItem
+                button
+                selected={selectedItem === 'SupportTicket'}
+                onClick={() => handleItemClick('SupportTicket')}
+                sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
+              >
+                <ListItemIcon>
+                  <ConfirmationNumberIcon sx={{ color: '#ecf0f1' }} />
+                </ListItemIcon>
+                <ListItemText primary="Support Ticket Management" />
+              </ListItem>
+              <ListItem
+                button
+                selected={selectedItem === 'PromotionManage'}
+                onClick={() => handleItemClick('PromotionManage')}
+                sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
+              >
+                <ListItemIcon>
+                  <LocalOfferIcon sx={{ color: '#ecf0f1' }} />
+                </ListItemIcon>
+                <ListItemText primary="Promotion Management" />
+              </ListItem>
+              <ListItem
+                button
+                selected={selectedItem === 'OrderPaymentManage'}
+                onClick={() => handleItemClick('OrderPaymentManage')}
+                sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}
+              >
+                <ListItemIcon>
                   <AllInboxIcon sx={{ color: '#ecf0f1' }} />
                 </ListItemIcon>
                 <ListItemText primary="Order Payment Management" />
-                </ListItem>
+              </ListItem>
             </>
           )}
         </List>
         <List sx={{ marginTop: 'auto' }}>
-          <ListItem button onClick={handleLogout} sx={{ '&:hover': { backgroundColor: '#EDAF28' } }}>
+          <ListItem button onClick={handleLogout} sx={{ '&:hover': { backgroundColor: '#2c3e50' } }}>
             <LogoutIcon>
-            <InventoryIcon sx={{ color: '#ecf0f1' }} /> 
+              <InventoryIcon sx={{ color: '#ecf0f1' }} />
             </LogoutIcon>
-            <ListItemText primary="Logout" sx={{paddingLeft:'50px'}}/>
+            <ListItemText primary="Logout" sx={{ paddingLeft: '50px' }} />
           </ListItem>
         </List>
       </Drawer>
