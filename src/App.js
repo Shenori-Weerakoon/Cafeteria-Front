f8ec0944f20c8d0813b7e8d4d6acfd6bc66dd83a
shenori <it22150448@my.sliit.lk>
2024-04-23 12:33:19 +0530

    update app.js with missing routes

--- src/App.js
+++ src/App.js
@@ -1,73 +1,66 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MenuManage from './components/Admin/MenuManage';
-import SupportTicket from './components/Admin/SupportTicket';
-import Feeback from './components/Customer/Feeback';
-import MenuForm from './components/Admin/MenuForm';
 import SignUp from './components/Customer/SignUp';
 import LogIn from './components/Customer/Login';
-import Profile from './components/Customer/Profile';
-import Customer from './components/Admin/Customer';
-import InventoryManage from './components/Admin/InventoryManage';
-import InventoryForm from './components/Admin/InventoryForm';
-import PromotionManage from './components/Admin/PromotionManage';
-import PromotionForm from './components/Admin/PromotionForm';
-import HomePage from './components/Customer/HomePage';
-import CheckoutPage from './components/Customer/Checkout';
 import AdminLogin from './components/Admin/AdminLogin';
 import AdminSignUp from './components/Admin/AdminSignUp';
 import Employee from './components/Admin/EmployeeManage';
+import MenuManage from './components/Admin/MenuManage';
+import MenuForm from './components/Admin/MenuForm';
 import EmployeeForm from './components/Admin/EmployeeForm';
-import EmployeeLogin from './components/Admin/EmployeeLogin';
-import EmployeeForm from './components/Admin/EmployeeForm';
-import CashOnDelivery from './components/Customer/CashOnDelivery';
-import OrderPaymentManage from './components/Admin/OrderPaymentManage';
-import Payment from './components/Customer/Payment';
-import Orders from './components/Customer/Orders';
+import InventoryManage from './components/Admin/InventoryManage';
+import InventoryForm from './components/Admin/InventoryForm';
 import SupportTicket from './components/Admin/SupportTicket';
+import PromotionManage from './components/Admin/PromotionManage';
+import OrderPaymentManage from './components/Admin/OrderPaymentManage';
+import PromotionForm from './components/Admin/PromotionForm';
+import SalaryForm from './components/Admin/SalaryForm';
+import EmployeeLogin from './components/Admin/EmployeeLogin';
+import HomePage from './components/Customer/HomePage';
+import AboutUs from './components/Customer/AboutUs';
+import MenuPage from './components/Customer/MenuPage';
+import CartPage from './components/Customer/CartPage';
 import Feeback from './components/Customer/Feeback';
 import Support from './components/Customer/Support';
-
-
+import Payment from './components/Customer/Payment';
+import CashOnDelivery from './components/Customer/CashOnDelivery';
+import Profile from './components/Customer/Profile';
+import CheckoutPage from './components/Customer/Checkout';
+import Orders from './components/Customer/Orders';
+import Customer from './components/Admin/Customer';
 
 
 function App() {
   return (
     <Router>
       <Routes>        
-        
-        <Route path="/MenuManage" element={<MenuManage />} />
-
-        <Route path="/Feeback" element={<Feeback/>} />
-        <Route path="/Support" element={<Support/>} />
-        
-
-        <Route path="/MenuForm" element={<MenuForm />} /> 
-        <Route path="/Login" element={<LogIn />} />
-        <Route path="/Register" element={<SignUp />} />
-        <Route path="/Profile" element={<Profile/>} />
+        <Route path="/OrderPaymentManage" element={<OrderPaymentManage />} />
         <Route path="/Customer" element={<Customer />} />
-        <Route path="/InventoryManage" element={<InventoryManage />} />
-        <Route path="/InventoryForm" element={<InventoryForm />} />
+        <Route path="/SalaryForm" element={<SalaryForm />} />
+        <Route path="/SupportTicket" element={<SupportTicket />} />
         <Route path="/PromotionForm" element={<PromotionForm />} />
         <Route path="/PromotionManage" element={<PromotionManage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/CheckoutPage" element={<CheckoutPage/>} />
-        <Route path="/SalaryForm" element={<SalaryForm />} />
+        <Route path="/InventoryManage" element={<InventoryManage />} />
+        <Route path="/InventoryForm" element={<InventoryForm />} />
+        <Route path="/MenuManage" element={<MenuManage />} />
+        <Route path="/MenuForm" element={<MenuForm />} />        
         <Route path="/EmployeeForm" element={<EmployeeForm />} />
         <Route path="/Employee" element={<Employee />} />        
         <Route path="/AdminLogin" element={<AdminLogin />} />
         <Route path="/AdminSignUp" element={<AdminSignUp />} />
-        <Route path="/EmployeeLogin" element={<EmployeeLogin/>} />
-        <Route path="/Payment" element={<Payment/>} />
-        <Route path="/CashOnDelivery" element={<CashOnDelivery/>} />
-        <Route path="/OrderPaymentManage" element={<OrderPaymentManage />} />
-        <Route path="/Orders" element={<Orders/>} />
-        <Route path="/SupportTicket" element={<SupportTicket />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Login" element={<LogIn />} />
+        <Route path="/Register" element={<SignUp />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/MenuPage" element={<MenuPage />} />
+        <Route path="/CartPage" element={<CartPage />} />
         <Route path="/Feeback" element={<Feeback/>} />
         <Route path="/Support" element={<Support/>} />
-
-
-
+        <Route path="/Payment" element={<Payment/>} />
+        <Route path="/CashOnDelivery" element={<CashOnDelivery/>} />
+        <Route path="/Profile" element={<Profile/>} />
+        <Route path="/CheckoutPage" element={<CheckoutPage/>} />
+        <Route path="/Orders" element={<Orders/>} />
+        <Route path="/EmployeeLogin" element={<EmployeeLogin/>} />
       </Routes>
     </Router>
   );
